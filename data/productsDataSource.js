e60747232f39eb7e1b124ce692e88c6afe50113e
JeremyBoisdur <jeremy.boisdur@digitregroup.com>
2020-09-07 16:48:50 +0200

    fix bug fetch

---
+++ data/productsDataSource.js
@@ -0,0 +1,31 @@
+const ProductApi = {
+    baseUrl: 'http://localhost:3000/api/teddies',
+
+    getProducts: async function () {
+        /*const products = await fetch(ProductApi.baseUrl)*/
+        // fetch une vrai api
+
+        return await fetch(ProductApi.baseUrl)
+        .then(function (response) {
+            if (!response.ok) {
+              throw new Error('HTTP error, status = ' + response.status);
+            }
+            return response.json();
+        })
+        .then(function (json) {
+            console.log(json)
+            return json;
+        })
+        .catch(function (error) {
+                console.log(error);
+        })
+    },
+
+    getProduct: function (id) {
+        console.log('Get a single product')
+    },
+
+    createOrder: function () {
+        console.log('Create an order')
+    }
+}
---
+++ main.js
@@ -0,0 +1,28 @@
+const Dom = {
+    teddiesList: document.getElementById('teddies'),
+    product: document.getElementById('template-product'),
+
+    buildTeddiesItems: async function () {
+        console.log('loading ...')
+        const products = await ProductApi.getProducts()
+        let id
+        console.log(products)
+
+        products.forEach((p, i) => {
+            const template = document.importNode(Dom.product.content, true)
+            const item = template.getElementById('product')
+            id = template.getElementById(('title'))
+            id.innerHTML = p._id
+            item.setAttribute('data-product-id', i)
+            item.addEventListener('click', Dom.showProductPage)
+            Dom.teddiesList.appendChild(template)
+        })
+        console.log(products)
+    },
+
+    showProductPage: function (e) {
+        const productId = e.currentTarget.dataset.productId
+        console.log(`je suis clické depuis le produit numéro ${productId}`)
+    }
+}
+window.onload = Dom.buildTeddiesItems()
