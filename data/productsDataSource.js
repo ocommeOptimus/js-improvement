d071c942ffb02bb4d412b5f0b20df0e8261b16d8
JeremyBoisdur <jeremy.boisdur@digitregroup.com>
2020-09-12 09:14:41 +0200

    fix HTML and JS structure for product page

--- data/productsDataSource.js
+++ data/productsDataSource.js
@@ -10,10 +10,10 @@ const ProductApi = {
         let urlStr = new URLSearchParams(queryStr)
         return 'http://localhost:3000/api/' + urlStr.get('type') + "/" + urlStr.get("id")
     },
-
+    // paramUrl: function (arr) {
+    //     return 'http://localhost:3000/api' + arr.param + "/" + arr.id
+    // },
     getProducts: async function (url) {
-        /*const products = await fetch(ProductApi.baseUrl)*/
-        // fetch une vrai api
 
         return await fetch(url)
         .then(function (response) {
@@ -30,24 +30,4 @@ const ProductApi = {
                 console.log(error);
         })
     },
-    
-    sending: function (url, order) {
-        return new Promise(function (resolve, reject) {
-            let request = new XMLHttpRequest();
-            request.onreadystatechange = function (response) {
-                if (this.readyState === 4) {
-                    if (this.status === 201) {
-                        resolve(response = JSON.parse(this.responseText), orderIds.push(new OrderConfirm(response.orderId, JSON.parse(localStorage.getItem('paramOrder')))), localStorage.setItem('orderId', JSON.stringify(orderIds)), localStorage.setItem('contact', JSON.stringify(response.contact)));
-                    } else {
-                        reject(
-                            alert(this.satus, + '\n' + 'Une erreur est survenue, merci de réessayer ultérieurement')
-                        )
-                    }
-                }
-            }
-            request.open("POST", url);
-            request.setRequestHeader("Content-Type", "application/json");
-            request.send(JSON.stringify(order));
-        })
-    }
 }
